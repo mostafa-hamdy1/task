b7986b7a07a1d107e3be2c7c7b6b2b6243ad21e9
Mostafa Hamdy <mostafa8026453@gmail.com>
2026-08-13 14:05:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day01Giza02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
